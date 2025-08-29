b5742aff29ec0d792aa5a2efe208886043334589
Arnab Senapati <133205153+Arnab-HITK@users.noreply.github.com>
2025-08-29 11:32:30 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,23 +9,22 @@ const Header = () => {
         <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
             Book Appointment <br /> With Trusted Doctors
         </p>
-        <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+        <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-medium'>
             <img className='w-28' src={assets.group_profiles} alt="" />
-            <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/>schedule your appointments hassle-free.
+            <p>Simply browse through our extensive list of trusted doctors, schedule your appointments hassle-free.
             </p>
         </div>
-        <a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-lg m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+        <a href="#speciality" className='flex items-center gap-2 bg-[#ef87ac] px-8 py-3 rounded-full text-gray-800 font-medium text-lg m-auto md:m-0 hover:scale-105 transition-all duration-300'>
             Book Appointment <img className='w-3' src={assets.arrow_icon} alt="" />
         </a>
         </div>
     
         {/* ------------ Right Side ---------------*/}
         <div className='md:w-1/2 relative flex justify-center items-center'>
-            <img className=' w-full max-w-xs md:max-w-full md:absolute bottom-0 h-auto rounded-lg mx-auto  ' src={assets.header_img} alt="" />
+            <img className=' w-full max-w-xs md:max-w-full md:absolute bottom-0 h-auto rounded-lg mx-auto ' src={assets.header_img} alt="" />
         </div>
     </div>
   )
 }
-
 
 export default Header
